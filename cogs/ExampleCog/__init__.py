f1496a9acbbf3cc537b4a71b9682bdb8cee8c218
Vozdyx <vozdyx.dev@gmail.com>
2023-02-10 21:41:00 +0300

    Clean `cogs` directory

--- cogs/ExampleCog/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cog import Example
-
-async def setup(bot):
-    await bot.add_cog(Example(bot))
-
--- cogs/ExampleCog/cog.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-
-class Example(commands.Cog):
-    bot: commands.Bot
-
-    def __init__(self, bot: commands.Bot) -> None:
-        self.bot = bot
-
